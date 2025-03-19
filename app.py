393fb88a6a5b6a0dd2fc61567d3b595ec4e480e1
kacperg <kacpergcloud@gmail.com>
2025-03-19 19:34:21 +0100

    git commit -m "PZ-17 <message>"

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from applications.file_downloader.downloader import downloader_blueprint
 from applications.file_uploader.uploader import uploader_blueprint
 from applications.calculator.calculator import calculator_blueprint
 from applications.video_player.player import player_blueprint
-
+from applications.notes.notes import notes_blueprint
 
 app = Flask(__name__ )
 app.secret_key = 'your_secret_key'
@@ -15,6 +15,7 @@ app.register_blueprint(uploader_blueprint, url_prefix='/uploader')
 
 app.register_blueprint(calculator_blueprint, url_prefix='/calculator')
 app.register_blueprint(player_blueprint, url_prefix='/player')
+app.register_blueprint(notes_blueprint, url_prefix='/notes')
 
 @app.route('/')
 def index():
@@ -22,7 +23,8 @@ def index():
         {'name': 'Download', 'icon': 'download_icon.png', 'url': url_for('downloader.download_page')},
         {'name': 'Upload', 'icon': 'upload_icon.png', 'url': url_for('uploader_blueprint.file_uploader')},
         {'name': 'Calculator', 'icon': 'calculator_icon.png', 'url': url_for('calculator.calculator')},
-        {'name': 'Video Player', 'icon': 'play_button.png', 'url': url_for('player.video_list')}
+        {'name': 'Video Player', 'icon': 'play_button.png', 'url': url_for('player.video_list')},
+        {'name': 'Notes', 'icon': 'notes_icon.png', 'url': url_for('notes.notes')},
         # Możemy tutaj dodawać kolejne aplikacje
     ]
     return render_template('index.html', applications=applications)
